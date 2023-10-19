d961506bf073ae9252c5dfa788f8809b38d0b977
Bruno Cabral <cabral.brc@gmail.com>
2023-10-18 20:14:44 -0700

    WindowCapture classes

--- dino.py
+++ dino.py
@@ -1,12 +1,13 @@
 import templateplay as tp
 
-region = (960, 150, 960, 200)
+region = (960, 150, 1920, 350)  # X1, Y1, X2, Y2
 
 template_images = tp.load_templates("assets")
+window_capture = tp.WindowCapture()
 
 count = 0
 while tp.running:
-    screen = tp.screenshot(region)
+    screen = window_capture.screenshot(region)
     template_positions = tp.find_templates(screen, template_images)
     tp.show(screen, template_positions)
     count += 1
