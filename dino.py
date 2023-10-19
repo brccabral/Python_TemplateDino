155392dcaf179efffcf5ab0db14d2d72ccea8abe
Bruno Cabral <cabral.brc@gmail.com>
2023-10-18 20:35:13 -0700

    exit code on key

--- dino.py
+++ dino.py
@@ -1,6 +1,7 @@
+import cv2
 import templateplay as tp
 
-region = (960, 150, 1920, 350)  # X1, Y1, X2, Y2
+region = (960, 150, 1850, 350)  # X1, Y1, X2, Y2
 
 template_images = tp.load_templates("assets")
 window_capture = tp.WindowCapture()
@@ -10,7 +11,6 @@ while tp.running:
     screen = window_capture.screenshot(region)
     template_positions = tp.find_templates(screen, template_images)
     tp.show(screen, template_positions)
-    count += 1
-    print(count)
-    if count >= 20:
+    if cv2.waitKey(1) == ord('q'):
+        cv2.destroyAllWindows()
         tp.running = False
